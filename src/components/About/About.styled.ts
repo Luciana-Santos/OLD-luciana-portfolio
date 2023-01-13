601338e690f3ea377c4351e34b70373a17f19495
Luciana Santos <luciydss@hotmail.com>
2023-01-13 16:18:44 -0300

    about section finished

---
+++ src/components/About/About.styled.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const AboutStyled = styled.section`
+  min-height: 100vh;
+  padding-block: 60px;
+`;
+
+export const Content = styled.div`
+  display: grid;
+  gap: 2.5rem;
+
+  @media (min-width: ${(props) => props.theme.breakpoints.minWidth.md}) {
+    align-items: center;
+    grid-template-columns: 2fr 1fr;
+    justify-content: space-between;
+  }
+`;
+
+export const LeftContent = styled.div`
+  display: grid;
+  gap: 1.25rem;
+`;
+
+export const RightContent = styled.div`
+  margin-inline: auto;
+  max-width: 400px;
+`;
+
+export const LinksNTechs = styled.div`
+  display: flex;
+  gap: 2.5rem;
+  margin-top: 1.875rem;
+`;
+
+export const Links = styled.ul`
+  display: grid;
+
+  a {
+    padding-block: 0.3125rem;
+  }
+`;
